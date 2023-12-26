9e757e2d5d35f4440420f0b09c18afcff65abd32
Martin Staadecker <machstg@gmail.com>
2023-12-26 00:43:48 -0500

    Get a template for the email sign in flow working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import '@material/web/button/filled-button.js'
 import '@material/web/textfield/outlined-text-field.js'
+import { useFirebaseApp } from './firebase';
+import { getAuth, sendSignInLinkToEmail } from 'firebase/auth';
+import { Show, createSignal } from 'solid-js';
 
 const suffix = "@mail.utoronto.ca"
 
@@ -14,26 +17,64 @@ declare module "solid-js" {
 }
 
 
+
+
 function App() {
+  const [success, setSuccess] = createSignal(false);
   let email: any;
+  const app = useFirebaseApp();
+  const auth = getAuth(app);
 
-  const onSubmit = (e: any) => {
-    e.preventDefault()
-    // const full_email = email.value + suffix;
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
 
+    const actionCodeSettings = {
+      // URL you want to redirect back to. The domain (www.example.com) for this
+      // URL must be in the authorized domains list in the Firebase Console.
+      url: 'https://pop-uoft.firebaseapp.com/',
+      handleCodeInApp: true,
+    };
+
+    try {
+      await sendSignInLinkToEmail(auth, email.value + suffix, actionCodeSettings);
+      // The link was successfully sent. Inform the user.
+      // Save the email locally so you don't need to ask the user for it again
+      // if they open the link on the same device.
+      console.log(`Email sent to ${email.value+suffix}`)
+      window.localStorage.setItem('emailForSignIn', email);
+      setSuccess(true);
+    } catch (error: any) {
+      // TODO handle error
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    }
   }
 
   return (
+
     <div class="grid place-items-center h-screen w-screen bg-background">
       <form onSubmit={onSubmit}>
         <div class="flex flex-col space-y-4 bg-surface p-8 text-left rounded-md drop-shadow place-content-center">
-          <md-outlined-text-field label="U of T Email" value="" ref={email} required suffix-text={suffix} pattern="[\\w\\d_.\\-]+" />
-          <div class="flex flex-row justify-end">
-            <md-filled-button onClick="submit">Get email link</md-filled-button>
-          </div>
+          <Show when={!success()} fallback={
+            <div class="flex flex-col space-y-4">
+              <p class="text-2xl font-bold">Check your email!</p>
+              <p class="text-lg">We've sent you a link to sign in.</p>
+              <p class="text-lg">If you don't see it, check your spam folder.</p>
+            </div>
+
+          }>
+            <md-outlined-text-field label="U of T Email" ref={email} required suffix-text={suffix} pattern="[\\w\\d_.\\-]+" />
+            <div class="flex flex-row justify-end">
+              <md-filled-button onClick="submit">Get email link</md-filled-button>
+            </div>
+          </Show>
+
         </div >
       </form>
     </div>
+
+
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,31 @@ import { render } from 'solid-js/web'
 import './index.css'
 import App from './App'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { FirebaseAppProvider } from './firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCnIpYjoP9LBwfEyEcMQgq4gd0vaa6BstA",
+    authDomain: "pop-uoft.firebaseapp.com",
+    projectId: "pop-uoft",
+    storageBucket: "pop-uoft.appspot.com",
+    messagingSenderId: "448097432689",
+    appId: "1:448097432689:web:1a82ae4b88d2f2e28a97d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = document.getElementById('root')
 
-render(() => <App />, root!)
+render(
+    () =>
+        <FirebaseAppProvider app={app}>
+            <App />
+        </FirebaseAppProvider>,
+    root!
+)
